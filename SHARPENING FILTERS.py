e1be65f386b3a2452956f5adb1bcff2dbde0cfb7
kowsalya <kowsalyam193@gmail.com>
2024-10-01 15:18:56 +0530

    Update SHARPENING FILTERS.py

--- SHARPENING FILTERS.py
+++ SHARPENING FILTERS.py
@@ -1,34 +1,30 @@
 ###Sharpening Filters
-# In[4]: Using Laplacian Kernal
+# In[1]: Using Laplacian Kernal
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image1 = cv2.imread('jackie.jpeg')
+image2 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+
+kernel3 = np.array([[0,1,0], [1, -4,1],[0,1,0]])
+image5 =cv2.filter2D(image2, -1, kernel3)
+plt.imshow(image5)
+plt.title('Laplacian Kernel')
+
+
+# In[2]:Using Laplacian Operator
 
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-image1=cv2.imread("red.jpg")
-image2=cv2.cvtColor(image1,cv2.COLOR_BGR2RGB)
-kernel=np.ones((11,11),np.float32)/121
-image3=cv2.filter2D(image2,-1,kernel)
-kernel2=np.array([[-1,-1,-1],])
-image3=cv2.filter2D(image2,)
-plt.figure(figsize=(,8))
-plt.subplot()
-plt.imshow(image2)
-plt.title("Original Image")
-plt.axis("off")
-plt.subplot(1,2,)
+import matplotlib.pyplot as plt
+image1 = cv2.imread('jackie.jpeg')
+image2 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
 
-
-# In[5]:Using Laplacian Operator
-
-
-laplacian=cv2.Laplacian(image2,)
-plt.figure(figsize=(8,8))
-plt.subplot()
-plt.imshow(image2)
-plt.title("Original Image")
-plt.axis("off")
-
+new_image = cv2.Laplacian (image2, cv2.CV_64F)
+plt.imshow(new_image)
+plt.title('Laplacian Operator')
 
 
 
